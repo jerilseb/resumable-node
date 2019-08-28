32816e7b6e76c754d49c67b76d743c9f1d1bd617
Jeril Sebastian <seb988@gmail.com>
2019-08-28 21:27:02 +0530

    cors allow header

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(multipart());
 // Uncomment to allow CORS
 app.use( (req, res, next) => {
    res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', '*');
    next();
 });
 
